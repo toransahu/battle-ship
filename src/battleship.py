1c1146f340e9d5c5582366e6d5c259174bdf98d4
TORAN <toran.sahu@yahoo.com>
2018-02-14 11:01:18 +0530

    added

---
+++ src/battleship.py
@@ -0,0 +1,7 @@
+def main():
+    for _ in range(7):
+        size_of_field = input("Please Enter Width (in integer) & Height (in alphabets) of battle field:\n")
+        number_of_ships = input("Please Enter Number of Ships:\n")
+
+    for _ in range(number_of_ships):
+        pass
