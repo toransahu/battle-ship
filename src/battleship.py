c068d30ae766898bff7129c240f1ced7fcd03972
toransahu <toran.sahu@yahoo.com>
2018-02-15 15:31:32 +0530

    complete

--- src/battleship.py
+++ src/battleship.py
@@ -1,135 +1,179 @@
-
 class Battle:
-    '''
-    SIZE_OF_FIELD = ''
-    NUMBER_OF_SHIPS = ''
-    TYPES_OF_SHIP = []
-    TARGET_PLAYER2 = ''
-    TARGET_PLAYER1 = ''
-    FIELD1 = []
-    FIELD2 = []
-    '''
+    """
+    Battle between 2 players.
+    """
 
-    SIZE_OF_FIELD = '5 E'
-    NUMBER_OF_SHIPS = '2'
-    TYPES_OF_SHIP = ['Q 1 1 A1 B2', 'P 2 1 D4 C3']
-    TARGETS_OF_PLAYER1 = 'A1 B2 B2 B3'
-    TARGETS_OF_PLAYER2 = 'A1 B2 B3 A1 D1 E1 D4 D4 D5 D5'
-    FIELD1 = []
-    FIELD2 = []
+    def __init__(self):
+        self.SIZE_OF_FIELD = ''
+        self.NUMBER_OF_SHIPS = ''
+        self.TYPES_OF_SHIP = []
+        self.TARGETS_OF_PLAYER1 = ''
+        self.TARGETS_OF_PLAYER2 = ''
+        self.FIELD1 = []
+        self.FIELD2 = []
 
-    @staticmethod
-    def get_input():
-        Battle.SIZE_OF_FIELD = input("Please Enter Width (in integer) & Height (in alphabets) of battle field:\n")
-        Battle.NUMBER_OF_SHIPS = int(input("Please Enter Number of Ships:\n"))
-        Battle.TYPES_OF_SHIP = []
-        for _ in range(Battle.NUMBER_OF_SHIPS):
-            Battle.TYPES_OF_SHIP.append(input("Enter types, size and co-ordinates of ships:\n"))
-        '''
-        TARGET_PLAYER2 = input("Player 1: Input target points at battle field of Player 2:\n")
-        TARGET_PLAYER1 = input("Player 2: Input target points at battle field of Player 1:\n")
-        '''
-    @staticmethod
-    def pre_process():
-        width, height = Battle.SIZE_OF_FIELD.strip().split()
+    def get_input(self):
+        """
+        Take required input from user.
+
+        :return: void
+        """
+        self.SIZE_OF_FIELD = input(
+            "Please Enter Width (in integer) & Height (in alphabets) of battle field:\n"
+        )
+        self.NUMBER_OF_SHIPS = int(input("Please Enter Number of Ships:\n"))
+        self.TYPES_OF_SHIP = []
+        for _ in range(self.NUMBER_OF_SHIPS):
+            self.TYPES_OF_SHIP.append(
+                input("Enter types, size and co-ordinates of ships:\n"))
+
+        self.TARGETS_OF_PLAYER1 = input(
+            "Player 1: Input target points at battle field of Player 2:\n")
+        self.TARGETS_OF_PLAYER2 = input(
+            "Player 2: Input target points at battle field of Player 1:\n")
+
+    def pre_process(self):
+        """
+        Preprocess the input data.
+
+        :return: void
+        """
+        width, height = self.SIZE_OF_FIELD.strip().split()
         width = int(width)
         height = int(ord(height)) - 64
-        Battle.FIELD1 = [[0 for _ in range(width)] for _ in range(height)]
-        Battle.FIELD2 = [[0 for _ in range(width)] for _ in range(height)]
-        for ship_details in Battle.TYPES_OF_SHIP:
+
+        # initialize field cells with 0
+        self.FIELD1 = [[0 for _ in range(width)] for _ in range(height)]
+        self.FIELD2 = [[0 for _ in range(width)] for _ in range(height)]
+
+        # update field cell values as per the input data
+        for ship_details in self.TYPES_OF_SHIP:
             category, x, y, cell1, cell2 = ship_details.strip().split()
             x, y = int(x), int(y)
             cell1 = (ord(cell1[0]) - 65, int(cell1[1]) - 1)
             cell2 = (ord(cell2[0]) - 65, int(cell2[1]) - 1)
-            # print(cell1, cell2)
+
             if category == 'P':
                 while x > 0:
-                    Battle.FIELD1[cell1[0]][cell1[1] + x - 1] = 1
-                    Battle.FIELD2[cell2[0]][cell2[1] + x - 1] = 1
+                    self.FIELD1[cell1[0]][cell1[1] + x - 1] = 1
+                    self.FIELD2[cell2[0]][cell2[1] + x - 1] = 1
                     x -= 1
                 while y > 0:
-                    Battle.FIELD1[cell1[0] + y - 1][cell1[1]] = 1
-                    Battle.FIELD2[cell2[0] + y - 1][cell2[1]] = 1
+                    self.FIELD1[cell1[0] + y - 1][cell1[1]] = 1
+                    self.FIELD2[cell2[0] + y - 1][cell2[1]] = 1
                     y -= 1
 
             elif category == 'Q':
                 while x > 0:
-                    Battle.FIELD1[cell1[0]][cell1[1] + x - 1] = 2
-                    Battle.FIELD2[cell2[0]][cell2[1] + x - 1] = 2
+                    self.FIELD1[cell1[0]][cell1[1] + x - 1] = 2
+                    self.FIELD2[cell2[0]][cell2[1] + x - 1] = 2
                     x -= 1
                 while y > 0:
-                    Battle.FIELD1[cell1[0] + y - 1][cell1[1]] = 2
-                    Battle.FIELD2[cell2[0] + y - 1][cell2[1]] = 2
+                    self.FIELD1[cell1[0] + y - 1][cell1[1]] = 2
+                    self.FIELD2[cell2[0] + y - 1][cell2[1]] = 2
                     y -= 1
 
-        Battle.TARGETS_OF_PLAYER1 = [(ord(cell[0])-65, int(cell[1])) for cell in
-                                     Battle.TARGETS_OF_PLAYER1.strip().split()]
-        Battle.TARGETS_OF_PLAYER2 = [(ord(cell[0]) - 65, int(cell[1])) for cell in
-                                     Battle.TARGETS_OF_PLAYER2.strip().split()]
+        # convert target cells into tuples of (x,y) co-ordinates
+        self.TARGETS_OF_PLAYER1 = [
+            (ord(cell[0]) - 65, int(cell[1]) - 1)
+            for cell in self.TARGETS_OF_PLAYER1.strip().split()
+        ]
+        self.TARGETS_OF_PLAYER2 = [
+            (ord(cell[0]) - 65, int(cell[1]) - 1)
+            for cell in self.TARGETS_OF_PLAYER2.strip().split()
+        ]
 
-        print(Battle.FIELD1)
-        print(Battle.FIELD2)
+    def fire(self, player, target):
+        """
+        Fire a missile at target field.
 
-        print(Battle.TARGETS_OF_PLAYER1)
-        print(Battle.TARGETS_OF_PLAYER2)
-
-    @staticmethod
-    def fire(player, target):
+        :param player: player no. (integer)
+        :param target: cell coordinates of opposition field (tuple)
+        :return: next turn of the player (integer)
+        """
         if player == 1:
             # hit
-            if Battle.FIELD2[target[0]][target[1]] > 0:
-                Battle.FIELD2[target[0]][target[1]] -= 1
-                print('Player-1 fires a missile with target '+chr(target[0] + 65)+ str(target[1]) +' which got hit')
-                del Battle.TARGETS_OF_PLAYER1[0]
+            if self.FIELD2[target[0]][target[1]] > 0:
+                self.FIELD2[target[0]][target[1]] -= 1
+                print('Player-1 fires a missile with target ' + chr(
+                    target[0] + 65) + str(target[1] + 1) + ' which got hit')
+                del self.TARGETS_OF_PLAYER1[0]
                 return 1
             # miss
             else:
-                print('Player-1 fires a missile with target ' + chr(target[0] + 65) + str(target[1]) + ' which got miss')
+                print('Player-1 fires a missile with target ' + chr(
+                    target[0] + 65) + str(target[1] + 1) + ' which got miss')
+                del self.TARGETS_OF_PLAYER1[0]
                 return 2
         elif player == 2:
             # hit
-            if Battle.FIELD1[target[0]][target[1]] > 0:
-                Battle.FIELD1[target[0]][target[1]] -= 1
-                print('Player-2 fires a missile with target ' + chr(target[0] + 65) + str(target[1]) + ' which got hit')
-                del Battle.TARGETS_OF_PLAYER2[0]
+            if self.FIELD1[target[0]][target[1]] > 0:
+                self.FIELD1[target[0]][target[1]] -= 1
+                print('Player-2 fires a missile with target ' + chr(
+                    target[0] + 65) + str(target[1] + 1) + ' which got hit')
+                del self.TARGETS_OF_PLAYER2[0]
                 return 2
             # miss
             else:
-                print('Player-2 fires a missile with target ' + chr(target[0] + 65) + str(target[1]) + ' which got miss')
+                print('Player-2 fires a missile with target ' + chr(
+                    target[0] + 65) + str(target[1] + 1) + ' which got miss')
+                del self.TARGETS_OF_PLAYER2[0]
                 return 1
 
-    @staticmethod
-    def war():
-        # won = False
+    def war(self):
+        """
+        Execute war between Player 1 & 2
+
+        :return: 0 (Prints result)
+        """
         turn_of_player = 1
+
         points_1 = 0
         points_2 = 0
-        while True:
+
+        for ship in self.FIELD1:
+            points_1 += sum(ship)
+        for ship in self.FIELD2:
+            points_2 += sum(ship)
+
+        while len(self.TARGETS_OF_PLAYER1) > 0 or len(
+                self.TARGETS_OF_PLAYER2) > 0:
             if turn_of_player == 1:
-                if len(Battle.TARGETS_OF_PLAYER1) > 0:
-                    turn_of_player = Battle.fire(1, Battle.TARGETS_OF_PLAYER1[0])
+                if len(self.TARGETS_OF_PLAYER1) > 0:
+                    turn_of_player = self.fire(1, self.TARGETS_OF_PLAYER1[0])
+                    if turn_of_player == 1:
+                        points_2 -= 1
                 else:
                     print('Player-1 no more missiles left to launch')
-                    break
+                    turn_of_player = 2
+
             if turn_of_player == 2:
-                if len(Battle.TARGETS_OF_PLAYER2) > 0:
-                    turn_of_player = Battle.fire(2, Battle.TARGETS_OF_PLAYER2[0])
+                if len(self.TARGETS_OF_PLAYER2) > 0:
+                    turn_of_player = self.fire(2, self.TARGETS_OF_PLAYER2[0])
+                    if turn_of_player == 2:
+                        points_1 -= 1
                 else:
                     print('Player-2 no more missiles left to launch')
-                    break
-        for ship in Battle.FIELD1:
-            points_1 += sum(ship)
-        for ship in Battle.FIELD2:
-            points_2 += sum(ship)
-        if points_1 == points_2:
-            print('Peace')
-        elif points_1 > points_2:
-            print('Player-1 won the battle')
-        else:
-            print('Player-2 won the battle')
+                    turn_of_player = 1
+
+            if points_2 == 0:
+                print('Player-1 won the battle')
+                return 0
+
+            elif points_1 == 0:
+                print('Player-2 won the battle')
+                return 0
+
+        if len(self.TARGETS_OF_PLAYER2) == 0 and len(
+                self.TARGETS_OF_PLAYER1) == 0:
+            if points_1 == points_2:
+                print('Peace')
+                return 0
 
 
-# Battle.get_input()
-Battle.pre_process()
-Battle.war()
-
+'''
+battle1 = Battle()
+battle1.get_input()
+battle1.pre_process()
+battle1.war()
+'''
